4ddad5fc35472f6716e0d52d291a60f723e9a0a7
Tianyu Gu <macdavid313@gmail.com>
2022-03-14 08:44:20 +1100

    switch back to ivy backend

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -23,8 +23,8 @@
        (company +auto +childframe)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ;; (ivy +fuzzy +prescient +icons)               ; a search engine for love and life
-       (vertico +icons)
+       (ivy +childframe +prescient +icons)               ; a search engine for love and life
+       ;; (vertico +icons)
 
        :ui
        ;;deft              ; notational velocity for Emacs
